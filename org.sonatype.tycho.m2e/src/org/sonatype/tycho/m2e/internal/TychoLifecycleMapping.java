d692a64dcd520208b723ccee5f599956e9aadc7c
ifedorenko <ifedorenko@839f2357-842e-4662-a642-f9b39ea64065>
2009-11-29 04:52:46 +0000

    Configure JDT to compile classes to maven output folder by default
    git-svn-id: https://svn.sonatype.com/repos/code/products/sonatype-studio/trunk/tycho-m2e@11041 839f2357-842e-4662-a642-f9b39ea64065

--- org.sonatype.tycho.m2e/src/org/sonatype/tycho/m2e/internal/TychoLifecycleMapping.java
+++ org.sonatype.tycho.m2e/src/org/sonatype/tycho/m2e/internal/TychoLifecycleMapping.java
@@ -1,12 +1,16 @@
 package org.sonatype.tycho.m2e.internal;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.maven.model.Plugin;
 import org.apache.maven.project.MavenProject;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
@@ -18,6 +22,7 @@ import org.eclipse.pde.internal.core.natures.PDE;
 import org.eclipse.pde.internal.core.util.CoreUtility;
 import org.maven.ide.eclipse.internal.project.CustomizableLifecycleMapping;
 import org.maven.ide.eclipse.project.IMavenProjectFacade;
+import org.maven.ide.eclipse.project.MavenProjectUtils;
 import org.maven.ide.eclipse.project.configurator.AbstractProjectConfigurator;
 import org.maven.ide.eclipse.project.configurator.IExtensionLifecycleMapping;
 import org.maven.ide.eclipse.project.configurator.ProjectConfigurationRequest;
@@ -40,7 +45,7 @@ public class TychoLifecycleMapping
         String packaging = mavenProject.getPackaging();
         if ( "eclipse-plugin".equals( packaging ) || "eclipse-test-plugin".equals( packaging ) )
         {
-            configurePDEBundleProject( project, monitor );
+            configurePDEBundleProject( project, mavenProject, monitor );
         }
         else if ( "eclipse-feature".equals( packaging ) )
         {
@@ -60,7 +65,7 @@ public class TychoLifecycleMapping
         }
     }
 
-    private void configurePDEBundleProject( IProject project, IProgressMonitor monitor )
+    private void configurePDEBundleProject( IProject project, MavenProject mavenProject, IProgressMonitor monitor )
         throws CoreException
     {
         // see org.eclipse.pde.internal.ui.wizards.plugin.NewProjectCreationOperation
@@ -78,6 +83,7 @@ public class TychoLifecycleMapping
         // PDE can't handle default JDT classpath
         IJavaProject javaProject = JavaCore.create( project );
         javaProject.setRawClasspath( new IClasspathEntry[0], true, monitor );
+        javaProject.setOutputLocation( getOutputLocation( project, mavenProject, monitor ), monitor );
 
         // see org.eclipse.pde.internal.ui.wizards.tools.UpdateClasspathJob
         IPluginModelBase model = PluginRegistry.findModel( project );
@@ -87,6 +93,17 @@ public class TychoLifecycleMapping
             // Avoid NPE for now, but users may have to invoke PDE->UpdateClasspath manually
             ClasspathComputer.setClasspath( project, model );
         }
+    }
+
+    private IPath getOutputLocation( IProject project, MavenProject mavenProject, IProgressMonitor monitor )
+        throws CoreException
+    {
+        File outputDirectory = new File( mavenProject.getBuild().getOutputDirectory() );
+        outputDirectory.mkdirs();
+        IPath relPath = MavenProjectUtils.getProjectRelativePath( project, mavenProject.getBuild().getOutputDirectory() );
+        IFolder folder = project.getFolder( relPath );
+        folder.refreshLocal( IResource.DEPTH_INFINITE, monitor );
+        return folder.getFullPath();
     }
 
     @Override
