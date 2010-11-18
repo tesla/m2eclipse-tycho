78b73cf9a5a8e13ebb65cf7b8d7e907fe6ae1399
Pascal Rapicault <Pascal@phsst-2.local>
2010-11-18 15:55:01 -0500

    Updating the content of master to 0.4.x content

--- org.sonatype.tycho.m2e/src/org/sonatype/tycho/m2e/internal/OsgiBundleProjectConfigurator.java
+++ org.sonatype.tycho.m2e/src/org/sonatype/tycho/m2e/internal/OsgiBundleProjectConfigurator.java
@@ -8,27 +8,20 @@
 
 package org.sonatype.tycho.m2e.internal;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Plugin;
-import org.apache.maven.model.PluginExecution;
 import org.apache.maven.plugin.MojoExecution;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.pde.core.project.IBundleProjectService;
 import org.eclipse.pde.internal.core.natures.PDE;
-import org.maven.ide.eclipse.core.MavenLogger;
 import org.maven.ide.eclipse.project.IMavenProjectFacade;
 import org.maven.ide.eclipse.project.configurator.AbstractBuildParticipant;
 import org.maven.ide.eclipse.project.configurator.AbstractProjectConfigurator;
@@ -47,8 +40,7 @@ public class OsgiBundleProjectConfigurator
     public void configure( ProjectConfigurationRequest request, IProgressMonitor monitor )
         throws CoreException
     {
-        IMavenProjectFacade facade = request.getMavenProjectFacade();
-        if ( isOsgiBundleProject( facade, monitor ) )
+        if ( isOsgiBundleProject( request.getMavenProjectFacade(), monitor ) )
         {
             IProject project = request.getProject();
             addNature( project, PDE.PLUGIN_NATURE, monitor );
@@ -64,32 +56,7 @@ public class OsgiBundleProjectConfigurator
             }
             description.setBuildSpec( newBuilders.toArray( new ICommand[newBuilders.size()] ) );
             project.setDescription( description, monitor );
-
-            IPath manifestPath = getManifestPath( facade, request.getMavenSession(), monitor );
-            if ( manifestPath != null && manifestPath.segmentCount() > 1 )
-            {
-                IBundleProjectService projectService = Activator.getDefault().getProjectService();
-                projectService.setBundleRoot( project, manifestPath.removeLastSegments( 1 ) );
-            }
         }
-    }
-
-    /**
-     * Returns project relative path of generated OSGi bundle manifest
-     */
-    private IPath getManifestPath( IMavenProjectFacade facade, MavenSession session, IProgressMonitor monitor )
-        throws CoreException
-    {
-        for ( MojoExecution execution : getExecutions( facade, session, monitor ) )
-        {
-            File location = getParameterValue( "manifestLocation", File.class, session, execution );
-            if ( location != null )
-            {
-                return facade.getProjectRelativePath( location.getAbsolutePath() );
-            }
-        }
-
-        return null;
     }
 
     private boolean isOsgiBundleProject( IMavenProjectFacade facade, IProgressMonitor monitor )
@@ -121,14 +88,8 @@ public class OsgiBundleProjectConfigurator
                     throws Exception
                 {
                     Set<IProject> projects = super.build( kind, monitor );
-                    IMavenProjectFacade facade = getMavenProjectFacade();
-                    MavenSession session = getSession();
-                    IPath manifestPath = getManifestPath( facade, session, monitor );
-                    if ( manifestPath != null )
-                    {
-                        IFile file = facade.getProject().getFile( manifestPath.append( "MANIFEST.MF" ) );
-                        file.refreshLocal( IResource.DEPTH_ZERO, monitor );
-                    }
+                    IFile file = getMavenProjectFacade().getProject().getFile( "META-INF/MANIFEST.MF" );
+                    file.refreshLocal( IResource.DEPTH_ZERO, monitor );
                     return projects;
                 }
             };
@@ -150,48 +111,6 @@ public class OsgiBundleProjectConfigurator
     private boolean isMavenBundlePluginMojo( String groupId, String artifactId )
     {
         return MOJO_GROUP_ID.equals( groupId ) && MOJO_ARTIFACT_ID.equals( artifactId );
-    }
-
-    // TODO this has to go to AbstractProjectConfigurator
-    private List<MojoExecution> getExecutions( IMavenProjectFacade facade, MavenSession session,
-                                               IProgressMonitor monitor )
-        throws CoreException
-    {
-        List<MojoExecution> executions = new ArrayList<MojoExecution>();
-        List<Plugin> plugins = facade.getMavenProject( monitor ).getBuildPlugins();
-        for ( Plugin plugin : plugins )
-        {
-            if ( !isMavenBundlePluginMojo( plugin ) )
-            {
-                continue;
-            }
-
-            if ( plugin.getVersion() == null )
-            {
-                try
-                {
-                    String version = maven.resolvePluginVersion( plugin.getGroupId(), plugin.getArtifactId(), session );
-                    plugin.setVersion( version );
-                }
-                catch ( CoreException ex )
-                {
-                    MavenLogger.log( ex );
-                    console.logError( "Failed to determine plugin version for " + plugin );
-                    continue;
-                }
-            }
-
-            for ( PluginExecution execution : plugin.getExecutions() )
-            {
-                for ( String goal : execution.getGoals() )
-                {
-                    MojoExecution exec = new MojoExecution( plugin, goal, execution.getId() );
-                    exec.setConfiguration( (Xpp3Dom) execution.getConfiguration() );
-                    executions.add( exec );
-                }
-            }
-        }
-        return executions;
     }
 
 }
